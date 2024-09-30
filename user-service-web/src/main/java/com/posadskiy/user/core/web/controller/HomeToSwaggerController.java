e39ba805df0e424e3377ce9d29ee07f45b303b2c
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2024-09-30 21:13:08 +0200

    Web module

---
+++ user-service-web/src/main/java/com/posadskiy/user/core/web/controller/HomeToSwaggerController.java
@@ -0,0 +1,24 @@
+package com.posadskiy.user.core.web.controller;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.uri.UriBuilder;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.swagger.v3.oas.annotations.Hidden;
+
+import java.net.URI;
+
+@Controller()
+@Secured(SecurityRule.IS_ANONYMOUS)
+class HomeToSwaggerController {
+
+    private final static URI SWAGGER_UI = UriBuilder.of("/swagger-ui").path("index.html").build();
+
+    @Get
+    @Hidden
+    HttpResponse<?> home() {
+        return HttpResponse.seeOther(SWAGGER_UI);
+    }
+}
