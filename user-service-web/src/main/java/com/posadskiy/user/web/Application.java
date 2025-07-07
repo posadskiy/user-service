3e937b2dbfaf9a9cbb891054d3ca2307cc84e837
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-07-07 20:17:18 +0200

    v0.1.2

--- user-service-web/src/main/java/com/posadskiy/user/web/Application.java
+++ user-service-web/src/main/java/com/posadskiy/user/web/Application.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.License;
 @OpenAPIDefinition(
     info = @Info(
         title = "user-service",
-        version = "0.1.1",
+        version = "${project.version}",
         description = "User service for operating user processes",
         license = @License(name = "MIT", url = "https://opensource.org/license/mit"),
         contact = @Contact(url = "https://posadskiy.com", name = "Dimitri Posadskiy", email = "support@posadskiy.com")
