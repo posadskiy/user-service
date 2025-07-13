cc3bbd82dcc45b2d77ae4ce11bee06b8dc2ef2e6
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-07-13 22:21:29 +0200

    Fix tests

---
+++ user-service-web/src/test/java/com/posadskiy/user/web/UserAuthenticationTest.java
@@ -0,0 +1,31 @@
+package com.posadskiy.user.web;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static io.micronaut.http.HttpStatus.UNAUTHORIZED;
+import static io.micronaut.http.MediaType.TEXT_PLAIN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@MicronautTest
+class UserAuthenticationTest {
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void accessingASecuredUrlWithoutAuthenticatingReturnsUnauthorized() {
+        HttpClientResponseException e = assertThrows(HttpClientResponseException.class, () -> {
+            client.toBlocking().exchange(HttpRequest.GET("/").accept(TEXT_PLAIN));
+        });
+
+        assertEquals(UNAUTHORIZED, e.getStatus());
+    }
+} 
